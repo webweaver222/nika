28d5dc16c446de4587ee832ca5dc68bb53866ee7
Alex Teisheira <fukirx@gmail.com>
2021-01-26 23:19:54 +0200

    loader fix

--- src/Components/pages/project/index.tsx
+++ src/Components/pages/project/index.tsx
@@ -5,6 +5,7 @@ import Loader from "../../loader";
 const ProjectWrapper = styled.div`
   img {
     width: 100%;
+    visibility: ${(props) => (props.loaded ? "visible" : "hidden")};
   }
 `;
 
@@ -12,15 +13,9 @@ const Project = ({ imgUrl }: { imgUrl: string }) => {
   const [imgLoaded, setLoaded] = useState(false);
 
   return (
-    <ProjectWrapper>
-      <img
-        src={imgUrl}
-        alt="easycycle"
-        onLoad={() => setLoaded(true)}
-        style={{ visibility: imgLoaded ? "visible" : "hidden" }}
-      />
-
+    <ProjectWrapper loaded={imgLoaded}>
       {!imgLoaded && <Loader />}
+      <img src={imgUrl} alt="easycycle" onLoad={() => setLoaded(true)} />
     </ProjectWrapper>
   );
 };
