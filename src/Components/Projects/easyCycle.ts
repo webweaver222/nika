b2e2696e081b34c10ff6a8412a4a3a3099068cb3
Alex Teisheira <fukirx@gmail.com>
2021-02-03 00:25:01 +0200

    megasport section and styled-components refactor prod

---
+++ src/Components/Projects/easyCycle.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import Project from "./project";
+
+const EasyCycle = styled(Project)`
+  background-color: #d64847;
+  margin-top: 20px;
+
+  .project-text {
+    display: ${(props: { easyCycleInView: boolean }) =>
+      props.easyCycleInView ? "block" : "none"};
+  }
+
+  img {
+    display: ${(props: { easyCycleInView: boolean }) =>
+      props.easyCycleInView ? "inline-block" : "none"};
+  }
+
+  img.easycycle1 {
+    bottom: -65px;
+    animation: fadeInUp 0.7s;
+  }
+
+  img.easycycle2 {
+    top: -65px;
+    animation: fadeInDown 0.7s;
+  }
+`;
+
+export default EasyCycle;
---
+++ src/Components/Projects/project.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const Project = styled.div`
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+  z-index: 0;
+
+  .project-text {
+    position: relative;
+    z-index: 2;
+    width: 350px;
+    margin-right: 70px;
+    animation: fadein 1s;
+
+    h2 {
+      font-size: 40px;
+      letter-spacing: 3px;
+    }
+    p {
+      font-size: 16px;
+      letter-spacing: 1px;
+      line-height: 25px;
+      padding: 0 60px 0 0;
+    }
+  }
+
+  .project-shading {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0);
+    transition: background-color 0.25s ease;
+    cursor: pointer;
+  }
+
+  .project-shading:hover {
+    background-color: rgba(0, 0, 0, 0.07);
+  }
+
+  .section-img {
+    position: relative;
+    z-index: 2;
+    height: 500px;
+  }
+
+  @media only screen and (min-width: 1600px) {
+    .project-text {
+      margin-right: 170px;
+    }
+  }
+`;
+export default Project;
