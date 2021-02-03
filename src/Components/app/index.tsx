2337b8ba81e0e9747912d5878fe17a094a7e181d
Alex Teisheira <fukirx@gmail.com>
2021-02-03 23:20:10 +0200

    prod config fix

--- src/Components/app/index.tsx
+++ src/Components/app/index.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import { Pages } from "../pages/Routes";
-import { ParallaxProvider } from "react-scroll-parallax";
+
 const App = function App() {
   return (
-    <ParallaxProvider>
-      <Switch>
-        {Pages.map((page, index) => {
-          return (
-            <Route
-              exact
-              path={page.link}
-              component={page.component}
-              key={index}
-            />
-          );
-        })}
-      </Switch>
-    </ParallaxProvider>
+    <Switch>
+      {Pages.map((page, index) => {
+        return (
+          <Route
+            exact
+            path={page.link}
+            component={page.component}
+            key={index}
+          />
+        );
+      })}
+    </Switch>
   );
 };
 
